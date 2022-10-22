d00a7fde56c62a9ef8073248962373b91ab1d602
blakecoding <hnamnguyen.hy@gmail.com>
2022-10-22 09:29:52 +0700

    init

---
+++ javascript/data.js
@@ -0,0 +1,8 @@
+const Data = [
+  { id: 1, title: "Title 12", description: "dump text", img: "https://picsum.photos/200" },
+  { id: 2, title: "Title 23", description: "dump text", img: "https://picsum.photos/300" },
+  { id: 3, title: "Title 34", description: "dump text 3", img: "https://picsum.photos/199" },
+  { id: 4, title: "Title 4", description: "dump text 4", img: "https://picsum.photos/201" },
+  { id: 5, title: "Title 512", description: "dump text 5", img: "https://picsum.photos/202" },
+  { id: 5, title: "Title 62", description: "dump text 6", img: "https://picsum.photos/203" },
+];
